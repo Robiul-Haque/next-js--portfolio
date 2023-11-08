bdc511272bc9da19550f41a52bfc99da1b7bc7a6
Your Name <robiulcoc420@gmail.com>
2023-11-08 12:42:28 +0600

    complete skill progress bar

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -7,8 +7,11 @@ import javascript from '../../../public/javascript.png';
 import react from '../../../public/react-js.png';
 import express from '../../../public/express-js.png';
 import mongo from '../../../public/mongo-db.png';
-import node from '../../../public/node-js.png';
 import next from '../../../public/next-js.png';
+import figma from '../../../public/figma.png';
+import xd from '../../../public/adobe-xd.png';
+import firebase from '../../../public/firebase.png';
+import circle from '../../../public/circle.png';
 
 const page = () => {
     return (
@@ -37,8 +40,6 @@ const page = () => {
                     </div>
                 </div>
             </div>
-
-            {/*Languages & Tools */}
             <div className="grid gap-9 md:grid-cols-2">
                 <div>
                     <h5 className="my-3 text-2xl font-bold">Language & Framework</h5>
@@ -108,14 +109,72 @@ const page = () => {
                         </div>
                     </div>
                 </div>
-
                 <div>
                     <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
                     <div className="my-2">
-                        {/* {tools.map((tool, i) => (
-                            <Bar value={tool} key={i} />
-                        ))} */}
-                        progress bar
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-1/2 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={figma} alt="HTML" width={25} height={25}></Image>
+                                <span className="ml-3 text-black">FIGMA</span>
+                            </div>
+                            <span className="mr-4">50%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-1/3 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={xd} alt="HTML" width={25} height={25}></Image>
+                                <span className="ml-3 text-black">FIGMA</span>
+                            </div>
+                            <span className="mr-4">30%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-3/4 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={firebase} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">FIREBASE</span>
+                            </div>
+                            <span className="mr-4">70%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-1/3 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={circle} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">FRAMER</span>
+                            </div>
+                            <span className="mr-4">30%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-1/3 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={circle} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">TAN STACK</span>
+                            </div>
+                            <span className="mr-4">30%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-2/5 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={circle} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">HOOK FORM</span>
+                            </div>
+                            <span className="mr-4">40%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-1/2 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={circle} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">REACT ROUTER</span>
+                            </div>
+                            <span className="mr-4">50%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-3/5 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={circle} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">JWT</span>
+                            </div>
+                            <span className="mr-4">60%</span>
+                        </div>
+                        <div className="flex justify-between items-center my-3 rounded-full border dark:bg-dark-300 dark:bg-black-500">
+                            <div className="flex items-center px-4 py-1 rounded-full w-2/5 bg-gradient-to-r from-green to-blue-500">
+                                <Image src={circle} alt="HTML" width={15} height={15}></Image>
+                                <span className="ml-3 text-black">SWIPER JS</span>
+                            </div>
+                            <span className="mr-4">40%</span>
+                        </div>
                     </div>
                 </div>
             </div>
